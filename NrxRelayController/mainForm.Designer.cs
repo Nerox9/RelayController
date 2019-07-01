4eff9acc4a50fc55440d41bafd9d8c91d0392d11
Berk Arslan <arslanberk09@gmail.com>
2019-07-01 22:02:32 +0300

    Add project files.

---
+++ NrxRelayController/mainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace NrxRelayController
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toggleSwitch1 = new NrxRelayController.ToggleSwitch();
+            this.toggleSwitch2 = new NrxRelayController.ToggleSwitch();
+            this.relayLabel1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // toggleSwitch1
+            // 
+            this.toggleSwitch1.ActiveColor = System.Drawing.Color.DarkRed;
+            this.toggleSwitch1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.toggleSwitch1.ButtonColor = System.Drawing.Color.Silver;
+            this.toggleSwitch1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toggleSwitch1.Location = new System.Drawing.Point(61, 52);
+            this.toggleSwitch1.Name = "toggleSwitch1";
+            this.toggleSwitch1.PassiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toggleSwitch1.Size = new System.Drawing.Size(28, 44);
+            this.toggleSwitch1.SwitchLabel = null;
+            this.toggleSwitch1.TabIndex = 0;
+            // 
+            // toggleSwitch2
+            // 
+            this.toggleSwitch2.ActiveColor = System.Drawing.Color.DarkRed;
+            this.toggleSwitch2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.toggleSwitch2.ButtonColor = System.Drawing.Color.Silver;
+            this.toggleSwitch2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toggleSwitch2.Location = new System.Drawing.Point(182, 52);
+            this.toggleSwitch2.Name = "toggleSwitch2";
+            this.toggleSwitch2.PassiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toggleSwitch2.Size = new System.Drawing.Size(28, 44);
+            this.toggleSwitch2.SwitchLabel = null;
+            this.toggleSwitch2.TabIndex = 1;
+            // 
+            // relayLabel1
+            // 
+            this.relayLabel1.AutoSize = true;
+            this.relayLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.relayLabel1.ForeColor = System.Drawing.Color.DarkRed;
+            this.relayLabel1.Location = new System.Drawing.Point(47, 99);
+            this.relayLabel1.Name = "relayLabel1";
+            this.relayLabel1.Size = new System.Drawing.Size(69, 20);
+            this.relayLabel1.TabIndex = 2;
+            this.relayLabel1.Text = "Relay 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(166, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Relay 2";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(277, 162);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.relayLabel1);
+            this.Controls.Add(this.toggleSwitch2);
+            this.Controls.Add(this.toggleSwitch1);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Nrx Relay Controller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ToggleSwitch toggleSwitch1;
+        private ToggleSwitch toggleSwitch2;
+        private System.Windows.Forms.Label relayLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NrxRelayController/mainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidLibrary;
+//using HidSharp;
+
+namespace NrxRelayController
+{
+    public partial class mainForm : Form
+    {
+        private const int VendorId  =  1155;
+        private const int ProductId = 22352;
+
+        private const int ReportLength = 65;
+        private HidDevice device;
+
+        private static readonly Random Random = new Random();
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            DeviceConnect();
+
+            toggleSwitch1.device = this.device;
+            toggleSwitch2.device = this.device;
+            toggleSwitch1.usbData = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            toggleSwitch2.usbData = new byte[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+        }
+
+        private int DeviceConnect()
+        {
+            device = HidDevices.Enumerate(VendorId, ProductId).FirstOrDefault();
+
+            if (device == null)
+            {
+                Console.WriteLine("No device connected");
+                return 1;
+            }
+            else
+            {
+                device.OpenDevice();
+
+                device.Inserted += DeviceAttachedHandler;
+                device.Removed += DeviceRemovedHandler;
+
+                device.MonitorDeviceEvents = true;
+
+                //device.ReadReport(ReadReportCallback);
+
+                //device.CloseDevice();
+
+                return 0;
+            }
+        }
+
+        private void ReadReportCallback(HidReport report)
+        {
+            Console.WriteLine("recv: {0}", string.Join(", ", report.Data.Select(b => b.ToString("X2"))));
+            device.ReadReport(ReadReportCallback);
+        }
+
+        private void DeviceAttachedHandler()
+        {
+            Console.WriteLine("Device attached.");
+            //device.ReadReport(ReadReportCallback);
+        }
+
+        private void DeviceRemovedHandler()
+        {
+            Console.WriteLine("Device removed.");
+        }
+    }
+}
